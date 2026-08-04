b949f12632d6e67913adfeea7e8eb0d7996fddf2
Manikandan M P <manikandanmperikamana@gmail.com>
2026-08-04 21:24:08 +0530

    feat: add users table and reference user_id in workouts schema
    Updated db.js database initialization.
    Added a SQL query to create a users table with id, username (constrained to be unique), and password columns.
    Updated the workouts table schema to include a user_id column linked as a FOREIGN KEY to the users table.

--- backend/db.js
+++ backend/db.js
@@ -8,16 +8,28 @@ const dbPromise = open({
 
 const initDB = async () => {
   const db = await dbPromise;
+  
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT UNIQUE,
+      password TEXT
+    )
+  `);
+
   await db.exec(`
     CREATE TABLE IF NOT EXISTS workouts (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
+      user_id INTEGER,
       date TEXT,
       type TEXT,
       duration TEXT,
       weight INTEGER,
-      reps INTEGER
+      reps INTEGER,
+      FOREIGN KEY (user_id) REFERENCES users (id)
     )
   `);
 };
+
 
 export { dbPromise, initDB };
